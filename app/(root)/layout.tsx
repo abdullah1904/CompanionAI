8d641c4e204734cbde0e461cb94b0811989d9e8f
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2025-02-04 16:30:52 +0500

    Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar' 
 import React from 'react'
 
 type Props = {
@@ -9,6 +10,9 @@ const RootLayout = ({ children }: Props) => {
     return (
         <div className='h-full'>
             <Navbar/>
+            <div className='hidden md:flex mt-16 w-20 flex-col fixed inset-y-0'>
+                <Sidebar/>
+            </div>
             <main className='md:pl-20 pt-16 h-full'>
                 {children}
             </main>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { cn } from '@/lib/utils'
 import { UserButton } from '@clerk/nextjs'
-import { Menu, Sparkles } from 'lucide-react'
+import { Sparkles } from 'lucide-react'
 import { Poppins } from 'next/font/google'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '@/components/ui/button'
-import { ModeToggle } from './mode-toggle'
+import { ModeToggle } from '@/components/ModeToggle'
+import MobileSidebar from './MobileSidebar'
 
 const font = Poppins({
     weight: "600",
@@ -15,9 +16,9 @@ const font = Poppins({
 
 const Navbar = () => {
     return (
-        <div className='fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary'>
+        <div className='fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16'>
             <div className='flex items-center'>
-                <Menu className="block md:hidden" />
+                <MobileSidebar/>
                 <Link href='/'>
                     <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
                         companion.ai
