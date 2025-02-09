93cfe3951bd21f1308cf5057aa4333f3663caa3c
Muhammad Abdullah Zahid <>
2025-02-10 02:24:26 +0500

    Companions List

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,40 @@
 import Categories from '@/components/Categories'
+import Companions from '@/components/Companions'
 import SearchInput from '@/components/SearchInput'
 import prismadb from '@/lib/db'
 
-const RootPage = async () => {
+type Props = {
+  searchParams:
+  Promise<{
+    categoryId: string,
+    name: string
+  }>
+}
+
+const RootPage = async ({ searchParams }: Props) => {
+  const { categoryId, name } = await searchParams;
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: categoryId,
+      name: {
+        search: name
+      }
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: { messages: true }
+      }
+    }
+  });
   const categories = await prismadb.category.findMany()
   return (
     <div className='h-full p-4 space-y-2'>
-      <SearchInput/>
-      <Categories data={categories}/>
+      <SearchInput />
+      <Categories data={categories} />
+      <Companions data={data} />
     </div>
   )
 }
